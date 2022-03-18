fb0bb7eeb4b474671628707cf8a80b56f75413b0
AteetGogineni <ateet.gogineni@yesgnome.com>
2022-03-18 16:47:40 +0530

    created prefab start tile, forward tile, right tile and spawining tile

---
+++ Assets/Scripts/TileSpawnManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpawnManager : MonoBehaviour
+{
+    public GameObject currentTile;
+    //public GameObject rightTile;
+    //public GameObject forwardTile;
+
+    public GameObject[] tilesPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            int k = Random.Range(0, 2);
+
+            currentTile = Instantiate(tilesPrefab[k], currentTile.transform.GetChild(k).position, Quaternion.identity);
+        }
+    }
+        
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
